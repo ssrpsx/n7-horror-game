471036e5a9f1af50b324cacbe6d88f326e289600
Sarawut Ponsan <phaiza237@gmail.com>
2025-07-17 21:10:24 +0700

    Update BG

--- code-game-godot/apple_bedroom.gd
+++ code-game-godot/apple_bedroom.gd
@@ -7,13 +7,13 @@ func _ready():
 	player_instance = player_scene.instantiate()
 	
 	# กำหนดตำแหน่งเกิดตรงนี้
-	player_instance.global_position = Vector2(-280, 110)  # เปลี่ยนเป็นตำแหน่งที่คุณต้องการ
+	player_instance.global_position = Vector2(180, 110)  # เปลี่ยนเป็นตำแหน่งที่คุณต้องการ
 
 	add_child(player_instance)
 	player_instance.z_index = 10
 
 	var camera = player_instance.get_node("player/Camera2D")
-	camera.limit_left = -415
-	camera.limit_right = 425
+	camera.limit_left = -190
+	camera.limit_right = 218
 	camera.limit_top = -120
 	camera.limit_bottom = 0
