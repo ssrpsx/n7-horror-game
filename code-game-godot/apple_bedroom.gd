b95371d0902c82e14619182cf9cc2d11360e54f8
Sarawut Ponsan <phaiza237@gmail.com>
2025-07-17 19:24:14 +0700

    Added 6, 7 th labroom and floor

---
+++ code-game-godot/apple_bedroom.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var player_scene = preload("res://player.tscn")
+var player_instance
+
+func _ready():
+	player_instance = player_scene.instantiate()
+	
+	# กำหนดตำแหน่งเกิดตรงนี้
+	player_instance.global_position = Vector2(-280, 110)  # เปลี่ยนเป็นตำแหน่งที่คุณต้องการ
+
+	add_child(player_instance)
+	player_instance.z_index = 10
+
+	var camera = player_instance.get_node("player/Camera2D")
+	camera.limit_left = -415
+	camera.limit_right = 425
+	camera.limit_top = -120
+	camera.limit_bottom = 0
