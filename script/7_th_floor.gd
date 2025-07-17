024d63c93eccebb7805fec21b1c07230a0508b6f
Sarawut Ponsan <phaiza237@gmail.com>
2025-07-17 21:49:14 +0700

    Update path

---
+++ script/7_th_floor.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var player_scene = preload("res://scenes/player.tscn")
+var player_instance
+
+func _ready():
+	player_instance = player_scene.instantiate()
+	
+	player_instance.global_position = Vector2(-280, 110)
+
+	add_child(player_instance)
+	player_instance.z_index = 10
+
+	var camera = player_instance.get_node("player/Camera2D")
+	camera.limit_left = -1259
+	camera.limit_right = 1269
+	camera.limit_top = -120
+	camera.limit_bottom = 0
---
+++ script/7_th_lab.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var player_scene = preload("res://scenes/player.tscn")
+var player_instance
+
+func _ready():
+	player_instance = player_scene.instantiate()
+	
+	player_instance.global_position = Vector2(-280, 110)
+
+	add_child(player_instance)
+	player_instance.z_index = 10
+
+	var camera = player_instance.get_node("player/Camera2D")
+	camera.limit_left = -415
+	camera.limit_right = 425
+	camera.limit_top = -120
+	camera.limit_bottom = 0
